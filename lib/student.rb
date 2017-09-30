0b7ef43cc409dde194db0f8e768410358d81c9b5
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-30 16:15:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,6 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
     sql= <<-SQL
       SELECT *
       FROM students
@@ -24,8 +22,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
@@ -87,15 +83,15 @@ class Student
     end
   end
 
-  def self.first_student_in_grade_10
+  def self.first_X_students_in_grade_10(X)
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDER BY students.id ASC LIMIT 1
+      ORDER BY students.id ASC LIMIT ?
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 end
