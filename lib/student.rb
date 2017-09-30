980b380682eaf7610bf76bf0a733ac84460cd96d
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-30 16:23:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,14 +108,14 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X(grade)
+  def self.all_students_in_grade_X(x)
     sql= <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade = grade
+      WHERE grade = x
     SQL
 
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
